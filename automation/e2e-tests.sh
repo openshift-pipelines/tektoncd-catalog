9ea18e0b1822e72a39d5522b3e1e633e7f66e61e
Vincent Demeester <vdemeest@redhat.com>
2023-02-22 10:24:29 +0100

    Import `buildah` Task from operator/upstream
    … and have a very very very minimal way to test it.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- automation/e2e-tests.sh
+++ automation/e2e-tests.sh
@@ -3,6 +3,8 @@
 set -ex
 
 echo "Run e2e tests :)"
-./automation/e2e-deploy-resources.sh
+./automation/e2e-deploy-resources.sh $1
 
-echo make cluster-test
+# TODO: handle platform arg
+# TODO: create an actual tool for this
+./tasks/buildah/0.6/tests/run.sh $1
