24575a6792a5c8c60887f7bcb5b09e21d791b1fb
Vincent Demeester <vdemeest@redhat.com>
2023-08-04 15:26:03 +0200

    e2e-tests.sh: do nothing for now..
    Let's just start from a blank "green" page :)
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- automation/e2e-tests.sh
+++ automation/e2e-tests.sh
@@ -5,6 +5,8 @@ set -ex
 echo "Run e2e tests :)"
 #./automation/e2e-deploy-resources.sh $1
 
+# TODO: handle the branch we are in – main and p might have different behavior
 # TODO: handle platform arg
 # TODO: create an actual tool for this
-./tasks/buildah/0.6/tests/run.sh $1
+echo "Nothing to do yet"
+# ./tasks/buildah/0.6/tests/run.sh $1
